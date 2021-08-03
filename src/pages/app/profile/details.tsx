52c97ad32ab07455afbd147512a9b36e6eeb92eb
Adam Turner <adam.turner@qualio.com>
2021-08-03 10:36:53 +0100

    ...

--- src/pages/app/profile/details.tsx
+++ src/pages/app/profile/details.tsx
@@ -4,7 +4,11 @@ import { Input, PageWrapper, Select } from '../../../components';
 import { EthnicGroups, Ethnicity, GenderIdentities, MilitaryService, Sex } from '../../../utils';
 import { v4 } from 'uuid';
 import { User } from '../../../../models';
-import { getUser, saveUser, updateUser } from '../../../services/User';
+import {
+  getUser,
+  // saveUser,
+  // updateUser
+} from '../../../services';
 import { useAuth } from '../../../hooks';
 
 export const PersonalDetails: React.FC = () => {
@@ -32,18 +36,18 @@ export const PersonalDetails: React.FC = () => {
   }, [reset]);
 
   const onSubmit = async (event: User) => {
-    let user: User = event;
+    const user: User = event;
     if (currentUser?.id) {
-      const input = {
-        ...event,
-      };
-      user = await updateUser(currentUser, input);
+      //   const input = {
+      //     ...event,
+      //   };
+      //   user = await updateUser(currentUser, input);
     } else {
-      const input = {
-        ...event,
-        id: v4(),
-      };
-      user = await saveUser(input);
+      // const input = {
+      //   ...event,
+      //   id: v4(),
+      // };
+      // user = await saveUser(input);
     }
 
     if (!getPersonalDetails()) {
--- src/services/user.ts
+++ src/services/user.ts
@@ -3,7 +3,7 @@ import { User } from '../../models';
 
 export const getUser = async () => (await DataStore.query(User))[0];
 
-export const saveUser = async (user: User) => await DataStore.save(new User(user));
+// export const saveUser = async (user: User) => await DataStore.save(new User(user));
 
-export const updateUser = async (initial: User, user: User) =>
-  await DataStore.save(User.copyOf(initial, (updated) => Object.assign(updated, user)));
+// export const updateUser = async (initial: User, user: User) =>
+//   await DataStore.save(User.copyOf(initial, (updated) => Object.assign(updated, user)));
