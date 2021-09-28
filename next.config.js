21516bcecc41eebbbbcb374aff1c0608781f740d
Adam Turner <adam.turner@qualio.com>
2021-09-28 22:04:40 +0100

    Added env var to next config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = withPWA({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  env: {
+    STRAVA__CLIENT_ID: process.env.STRAVA__CLIENT_ID,
+    STRAVA__CLIENT_SECRET: process.env.STRAVA__CLIENT_SECRET,
+    STRAVA__VERIFY: process.env.STRAVA__VERIFY,
+    STRAVA__REDIRECT: process.env.STRAVA__REDIRECT,
+  },
 });
 
 module.exports = nextConfig;
