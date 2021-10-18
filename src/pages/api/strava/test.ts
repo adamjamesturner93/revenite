190c9b22734e20473015f906dc31abd9e946a462
Adam Turner <adam.turner@qualio.com>
2021-10-18 08:27:00 +0100

    Fix test endpoint

--- src/pages/api/strava/test.ts
+++ src/pages/api/strava/test.ts
@@ -1,5 +1,5 @@
-import type { NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default (res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   return res.status(200).send({ url: process.env.STRAVA__REDIRECT });
 };
